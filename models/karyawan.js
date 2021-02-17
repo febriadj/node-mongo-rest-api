cda5c39aa00667123ccec04233cd31cae78fd9ce
i'mfebri <iamfebriadji@gmail.com>
2021-02-17 16:31:17 +0700

    update controllers

--- models/karyawan.js
+++ models/karyawan.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-exports.dbKaryawan = mongoose.model('karyawan', new Schema({
+module.exports = dbKaryawan = mongoose.model('karyawan', new Schema({
   nik: { type: String, unique: true },
-  nama: { first: String, last: String },
+  nama: { awal: String, akhir: String },
   umur: Number,
   posisi: String,
 }))
--- routes/controllers.js
+++ routes/controllers.js
@@ -2,7 +2,43 @@ const router = require('express').Router()
 const dbKaryawan = require('../models/karyawan')
 
 router.get('/', (req, res, next) => {
-  res.json({ body: 'hello world' })
+  dbKaryawan.find().sort({ nik: - 1 })
+    .then(result => res.json({ body: result }))
+    .catch(err => console.log(err))
+})
+
+router.post('/', (req, res, next) => {
+  const { nik, namaAwal, namaAkhir, umur, posisi } = req.body
+
+  if ( !nik || !namaAwal || !namaAkhir || !umur || !posisi ) return console.log('data ada yang belum ditambahkan')
+
+  const karyawan = new dbKaryawan({
+    nik: nik,
+    nama: { awal: namaAwal, akhir: namaAkhir },
+    umur: umur,
+    posisi: posisi
+  })
+  
+  karyawan.save()
+    .then(result => res.json({ body: 'Data berhasil ditambahkan' }))
+    .catch(err => { 
+      console.log(err)
+      res.status(401).json({ body: 'Gagal menambahkan data' })
+    })
+})
+
+router.delete('/', (req, res, next) => {
+  const { nik } = req.body
+  dbKaryawan.findOneAndDelete({ nik: nik })
+    .then(result => {
+      if ( result ) return res.json({ body: 'Data berhasil dihapus' })
+      console.log('Data tidak ditemukan')
+      res.sendStatus(403)
+    })
+    .catch(err => {
+      console.log(err)
+      res.sendStatus(401)
+    })
 })
 
 module.exports = router
